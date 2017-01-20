359b79bd661eea56eed7071cec1046e45edcd2d2
Trevor D. Miller <trevordmiller@icloud.com>
2017-01-20 02:55:07 -0400

    Add app icons (#4)

--- src/components/Screen/index.js
+++ src/components/Screen/index.js
@@ -38,6 +38,14 @@ export const Screen = ({
       <style>
         {styleReset}
       </style>
+      <link rel='manifest' href='/static/manifest.json' />
+      <link rel='apple-touch-icon' sizes='180x180' href='/static/apple-touch-icon.png' />
+      <link rel='mask-icon' href='/static/safari-pinned-tab.svg' color={uiGroups.userCurrentState} />
+      <meta name='theme-color' content={uiGroups.userCurrentState} />
+      <meta name='msapplication-config' content='/static/browserconfig.xml' />
+      <link rel='icon' type='image/png' href='/static/favicon-32x32.png' sizes='32x32' />
+      <link rel='icon' type='image/png' href='/static/favicon-16x16.png' sizes='16x16' />
+      <link rel='shortcut icon' href='/static/favicon.ico' />
     </Head>
 
     <div style={{
