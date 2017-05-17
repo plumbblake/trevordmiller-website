d1a34a9a8248e27780750d4266bf7255ff123a5a
Trevor D. Miller <trevordmiller@icloud.com>
2017-05-17 09:12:15 -0600

    Update blog post order

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -4,12 +4,6 @@ import Teaser from '../../components/Teaser'
 
 export const posts = {
 
-  'wholesome-media': {
-    mainVisual: '/static/opendns-porn.jpg',
-    title: `Configuring devices for wholesome media`,
-    description: `I use a combination of filters and settings to help keep media in my home wholesome.`,
-  },
-
   'color-scheme-building': {
     mainVisual: '/static/nova-example-code-screenshot.png',
     title: `Building the Nova color scheme`,
@@ -28,18 +22,23 @@ export const posts = {
     description: `For years I had a cheap office: a $30 plastic folding desk and an external monitor I'd plug into and that was it. But recently, I took the plunge and set up my dream office and I'm ecstatic with how it turned out.`,
   },
 
-  'rebuilding-my-website': {
-    mainVisual: '/static/trevordmiller-website.jpg',
-    title: `Rebuilding my website to aggregate all my stuff`,
-    description: `Since I started programming in high school, I've accumulated personal domains, websites, and apps spread across the internet. I decided I wanted to consolidate these into a single app with a shared set of components, so that it is easier to maintain. So now all my web projects live under https://trevordmiller.com; this includes my portfolio, open source project documentation, guitar lessons, blog etc.`,
-  },
-
   'spaced-repetition-software': {
     mainVisual: '/static/anki-card-question.jpg',
     title: `Retaining what I learn through Spaced Repetition Software (SRS)`,
     description: `When I learn something new, I add it to my SRS. Then, each morning I review what my SRS suggests I\'m about to forget, to ingrain it in my long-term memory.`,
   },
 
+  'wholesome-media': {
+    mainVisual: '/static/opendns-porn.jpg',
+    title: `Configuring devices for wholesome media`,
+    description: `I use a combination of filters and settings to help keep media in my home wholesome.`,
+  },
+
+  'rebuilding-my-website': {
+    mainVisual: '/static/trevordmiller-website.jpg',
+    title: `Rebuilding my website to aggregate all my stuff`,
+    description: `Since I started programming in high school, I've accumulated personal domains, websites, and apps spread across the internet. I decided I wanted to consolidate these into a single app with a shared set of components, so that it is easier to maintain. So now all my web projects live under https://trevordmiller.com; this includes my portfolio, open source project documentation, guitar lessons, blog etc.`,
+  },
 }
 
 const postKeys = Object.keys(posts)
