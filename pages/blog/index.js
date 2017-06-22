c466dc4a49fab742172e11c0849bdb722c6dc726
Trevor D. Miller <trevordmiller@icloud.com>
2017-06-21 22:30:49 -0600

    Update blog post date

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -10,7 +10,7 @@ export const posts = {
     mainVisual: '/static/front-end-tooling-preferences.jpg',
     title: `My current front-end tooling preferences`,
     description: `An opinionated list of my current favorite options for front-end code projects. This includes the reasoning behind why I picked each option and an example project to show how they can all fit together.`,
-    date: 'June 14, 2017',
+    date: 'June 21, 2017',
   },
 
   'raspberry-pi-button': {
