343dd1808acd2aa92f5f7a631f20dd2b3aeff1e5
Trevor D. Miller <trevordmiller@icloud.com>
2018-02-19 02:09:53 -0700

    Simplify about wording

--- pages/about/index.js
+++ pages/about/index.js
@@ -10,7 +10,7 @@ const lifeAspects = [
   {
     visual: 'terminal',
     label: `coder`,
-    description: `I work as a software developer. I love learning and solving problems. I strive to learn new things every day and share what I learn.`,
+    description: `I'm a software developer. I love learning and solving problems. I strive to learn new things every day and share what I learn.`,
     links: [
       {
         description: 'Watch my video courses',
