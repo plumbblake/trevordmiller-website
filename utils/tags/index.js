436ae8d5844c0c9a374f52d575468e69e7d54baf
Trevor D. Miller <trevordmiller@icloud.com>
2018-03-30 11:36:35 -0600

    Uppercase tags

--- utils/tags/index.js
+++ utils/tags/index.js
@@ -2,77 +2,77 @@ import { uiGroups, syntaxGroups } from '../theme'
 
 const tags = {
   inProgress: {
-    label: 'in-progress',
+    label: 'In Progress',
     color: uiGroups.userCurrentState,
     icon: 'timer',
     order: 0,
   },
 
   terminal: {
-    label: 'terminal',
+    label: 'Terminal',
     color: syntaxGroups.type,
     icon: 'terminal',
     order: 1,
   },
 
   git: {
-    label: 'git',
+    label: 'Git',
     color: syntaxGroups.type,
     icon: 'git',
     order: 2,
   },
 
   vim: {
-    label: 'vim',
+    label: 'Vim',
     color: syntaxGroups.type,
     icon: 'vim',
     order: 3,
   },
 
   postgresql: {
-    label: 'postgresql',
+    label: 'PostgreSQL',
     color: syntaxGroups.type,
     icon: 'postgresql',
     order: 4,
   },
 
   graphQl: {
-    label: 'graphql',
+    label: 'GraphQL',
     color: syntaxGroups.type,
     icon: 'graphql',
     order: 5,
   },
 
   ruby: {
-    label: 'ruby',
+    label: 'Ruby',
     color: syntaxGroups.type,
     icon: 'ruby',
     order: 6,
   },
 
   node: {
-    label: 'node',
+    label: 'Node',
     color: syntaxGroups.type,
     icon: 'node',
     order: 7,
   },
 
   react: {
-    label: 'react',
+    label: 'React',
     color: syntaxGroups.type,
     icon: 'react',
     order: 8,
   },
 
   hardware: {
-    label: 'hardware',
+    label: 'Hardware',
     color: syntaxGroups.type,
     icon: 'plug',
     order: 9,
   },
 
   general: {
-    label: 'general',
+    label: 'General',
     color: syntaxGroups.type,
     icon: 'tag',
     order: 10,
