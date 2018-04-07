3d81332540f4806fa1358f1592e4865c80084890
Trevor D. Miller <trevordmiller@icloud.com>
2018-04-07 13:26:09 -0600

    Clean up blog index page

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -8,7 +8,7 @@ export const blogPosts = {
     tags: [tags.hardware],
     mainVisual: '/static/macbook.jpg',
     title: `Running Windows on Mac`,
-    description: `How to set up Windows on a Mac legally, for free, with native performance.`,
+    description: `How to set up Windows on a Mac legally, for free, with native performance using Boot Camp.`,
     date: 'April 6, 2018',
   },
 
@@ -16,7 +16,7 @@ export const blogPosts = {
     tags: [tags.css],
     mainVisual: '/static/brittle-css.jpg',
     title: `How to find and fix brittle CSS`,
-    description: `This article shows examples of making CSS easier to maintain with Flexbox, fluid values, additive media queries etc.`,
+    description: `Examples of making CSS easier to maintain with Flexbox, fluid values, additive media queries etc.`,
     date: 'March 12, 2018',
     links: [
       {
@@ -67,7 +67,7 @@ export const blogPosts = {
     tags: [tags.react],
     mainVisual: '/static/react-render-callback-components.png',
     title: `Sharing stateful UI logic in React apps using Render Callback components`,
-    description: `A few examples of abstracting shared logic across multiple components using the Render Callback pattern (aka Render Props or Function as Child components).`,
+    description: `Examples of abstracting shared logic across multiple React components using the Render Callback pattern (AKA Render Props or Function as Child components).`,
     date: 'July 15, 2017',
   },
 
@@ -75,7 +75,7 @@ export const blogPosts = {
     tags: [tags.terminal, tags.hardware, tags.node],
     mainVisual: '/static/dog-bathroom-button.jpg',
     title: `Running a server on a Raspberry Pi with a physical button`,
-    description: `All the pieces needed to set up a Raspberry Pi with a physical button. Covers buying hardware, running a Raspberry Pi, wiring up a physical button, and creating a server to send a text message when the button is pressed.`,
+    description: `How to set up a Raspberry Pi to send a text message when a button is pressed.`,
     date: 'June 1, 2017',
   },
 
@@ -83,7 +83,7 @@ export const blogPosts = {
     tags: [tags.terminal, tags.vim, tags.git, tags.node],
     mainVisual: '/static/nova-example-code-screenshot.png',
     title: `Building the Nova color scheme`,
-    description: `By using an module for color values, we were able to generate color schemes for multiple editors and tools from one source.`,
+    description: `Using an npm module to generate color schemes for multiple editors and tools from one source.`,
     date: 'May 1, 2017',
   },
 
@@ -91,7 +91,7 @@ export const blogPosts = {
     tags: [tags.general],
     mainVisual: '/static/todoist.jpg',
     title: `Using my task list as my single source of truth`,
-    description: `I like to stay organized with an automated task list so that I don’t forget to do the things I want and need to do, and so I can keep my brain free from remembering it all.`,
+    description: `How I use my task list to aggregate everything I want and need to do.`,
     date: 'April 1, 2017',
   },
 
@@ -99,15 +99,15 @@ export const blogPosts = {
     tags: [tags.general],
     mainVisual: '/static/minimal-office-first-person.jpg',
     title: `My minimal office set up`,
-    description: `I set up my dream office and I'm ecstatic with how it turned out.`,
+    description: `How I set up my office for focus and productivity.`,
     date: 'March 1, 2017',
   },
 
   'spaced-repetition-software': {
     tags: [tags.general],
     mainVisual: '/static/anki-card-question.jpg',
-    title: `Retaining what I learn through Spaced Repetition Software (SRS)`,
-    description: `When I learn something new, I add it to my SRS. Then, each morning I review what my SRS suggests I'm about to forget, to ingrain it in my long-term memory.`,
+    title: `Retaining what I learn through Spaced Repetition Software`,
+    description: `How I record and review what I learn to engrain it in long-term memory.`,
     date: 'February 1, 2017',
   },
 
@@ -115,7 +115,7 @@ export const blogPosts = {
     tags: [tags.general],
     mainVisual: '/static/common-sense-media.png',
     title: `Configuring devices for wholesome media`,
-    description: `How I use a combination of filters and settings to help keep media in my home wholesome.`,
+    description: `How I use a combination of filters and settings to keep media in my home clean.`,
     date: 'January 1, 2017',
   },
 
@@ -123,7 +123,7 @@ export const blogPosts = {
     tags: [tags.general],
     mainVisual: '/static/trevordmiller-website.png',
     title: `Rebuilding my website to aggregate all my stuff`,
-    description: `How I consolidated all of my code projects under one website.`,
+    description: `How I consolidate all of my code projects under one website.`,
     date: 'December 1, 2016',
   },
 }
