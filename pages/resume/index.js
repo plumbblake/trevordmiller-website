ba4e6b3990068ebb19dc0ddd6b32aaf29bd06211
Trevor D. Miller <trevordmiller@icloud.com>
2018-04-07 19:45:31 -0600

    Fix react map key error

--- pages/resume/index.js
+++ pages/resume/index.js
@@ -169,6 +169,7 @@ export default () => (
                     },
                   ].map(handle => (
                     <div
+                      key={handle.icon}
                       style={{
                         display: 'flex',
                         alignItems: 'center',
