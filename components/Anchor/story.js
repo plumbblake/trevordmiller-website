7d77278ad0dd43b0c010cdaa223c60b55474bd63
Trevor D. Miller <trevordmiller@icloud.com>
2018-06-30 10:36:21 -0600

    Add storybook info decorator for source code + proptypes docs

--- components/Anchor/story.js
+++ components/Anchor/story.js
@@ -2,6 +2,10 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import Anchor from ".";
 
-storiesOf("Anchor", module).add("default", () => (
-  <Anchor href="https://google.com">Some children</Anchor>
-));
+storiesOf("Anchor", module)
+  .add("Relative with prefetch", () => (
+    <Anchor href="/blog/example">Some children</Anchor>
+  ))
+  .add("Absolute", () => (
+    <Anchor href="https://google.com">Some children</Anchor>
+  ));
