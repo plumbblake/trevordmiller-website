dd94a702ce57168e82b817b1d102e7e99bf33f1e
Trevor D. Miller <trevordmiller@icloud.com>
2018-07-14 17:47:43 -0600

    Fail tests on any console warnings/errors

--- jest.setup.js
+++ jest.setup.js
@@ -5,3 +5,11 @@ import Router from "next/router";
 configure({ adapter: new Adapter() });
 
 Router.router = { push: () => {}, prefetch: () => {} };
+
+global.console.warning = message => {
+  throw new Error(message);
+};
+
+global.console.error = message => {
+  throw new Error(message);
+};
