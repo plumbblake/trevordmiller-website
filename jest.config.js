546aef694ed838573437d8b92d1a1e5925e69efb
Trevor D. Miller <trevordmiller@icloud.com>
2018-07-24 12:48:31 -0600

    Add modjulePathIgnorePatterns to speed up jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   setupFiles: ["<rootDir>/jest.setup.js"],
+  modulePathIgnorePatterns: [
+    "<rootDir>/node_modules/",
+    "<rootDir>/.next/",
+    "<rootDir>/storybook-static/"
+  ],
   testPathIgnorePatterns: [
     "<rootDir>/node_modules/",
     "<rootDir>/.next/",
