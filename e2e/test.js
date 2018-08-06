c5ff52f55ad21c7c8462015a32c7e2f71570da48
Trevor D. Miller <trevordmiller@icloud.com>
2018-08-05 19:08:32 -0600

    Add e2e test for joining courses

--- e2e/test.js
+++ e2e/test.js
@@ -12,26 +12,62 @@ beforeAll(async () => {
       ? {
           headless: false,
           slowMo: 50,
-          devtools: true
+          devtools: true,
+          dumpio: true
         }
       : {}
   );
+
   page = await browser.newPage();
+
+  page.on("error", e => {
+    throw new Error(e);
+  });
+
+  page.on("pageerror", e => {
+    throw new Error(e);
+  });
+
+  page.on("console", message => {
+    if (["error", "warning"].includes(message._type)) {
+      throw new Error(message._text);
+    }
+  });
 });
 
 const appUrl = process.env.APP_URL || "http://localhost:3000";
 
 const routes = {
-  home: `${appUrl}`
+  home: `${appUrl}`,
+  courses: `${appUrl}/courses`,
+  course: `${appUrl}/courses/real-world-git`,
+  blog: `${appUrl}/blog`,
+  blogPost: `${appUrl}/blog/rebuilding-my-website`,
+  projects: `${appUrl}/projects`,
+  resume: `${appUrl}/resume`,
+  about: `${appUrl}/about`,
+  follow: `${appUrl}/follow`
 };
 
-const timestamp = new Date().getUTCMilliseconds();
+const timestamp = new Date().valueOf();
 
 const user = {
   email: `trevordmillertest+${timestamp}@gmail.com`
 };
 
-test("can join email list", async () => {
+const testRoutes = () => {
+  for (const route of Object.keys(routes)) {
+    test(`can load ${route} route`, async () => {
+      await page.goto(routes[route]);
+      const content = '[data-testid="main"]';
+      await page.waitForSelector(content);
+    });
+  }
+};
+
+testRoutes();
+
+test.skip("can join the email list", async () => {
   await page.goto(routes.home);
 
   const button = '[data-testid="joinEmailListButton"]';
@@ -50,6 +86,16 @@ test("can join email list", async () => {
   await page.waitForSelector(mailchimp);
   const mailchimpContent = await page.$eval(mailchimp, el => el.innerText);
   expect(mailchimpContent).toContain("Subscription Confirmed");
+});
+
+test("can join courses", async () => {
+  await page.goto(routes.course);
+
+  const button = '[data-testid="joinCourseButton"]';
+  await page.waitForSelector(button);
+  await page.click(button);
+
+  expect(page.url()).toContain("https://sso.teachable.com");
 });
 
 afterAll(() => {
