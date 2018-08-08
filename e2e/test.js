cedab746be14357d33fa1ffc3729956efa5968af
Trevor D. Miller <trevordmiller@icloud.com>
2018-08-07 21:45:40 -0600

    Add describe label with appUrl e2e tests run on

--- e2e/test.js
+++ e2e/test.js
@@ -57,97 +57,102 @@ const user = {
   email: `trevordmillertest+${timestamp}@gmail.com`
 };
 
-describe("routes", () => {
-  const testRoutes = () => {
-    for (const route of Object.keys(routes)) {
-      test(`can load ${route} route`, async () => {
-        await page.goto(routes[route]);
-        const content = '[data-testid="main"]';
-        await page.waitForSelector(content);
+describe(`e2e tests on ${appUrl}`, () => {
+  describe("routes", () => {
+    const testRoutes = () => {
+      for (const route of Object.keys(routes)) {
+        test(`can load ${route} route`, async () => {
+          await page.goto(routes[route]);
+          const content = '[data-testid="main"]';
+          await page.waitForSelector(content);
+        });
+      }
+    };
+
+    testRoutes();
+  });
+
+  describe("subscribing", () => {
+    if (isProduction) {
+      test("can join the email list", async () => {
+        await page.goto(routes.home);
+
+        const button = '[data-testid="joinEmailListButton"]';
+        await page.waitForSelector(button);
+        await page.click(button);
+
+        const input = '[data-testid="joinEmailListInput"]';
+        await page.waitForSelector(input);
+        await page.type(input, user.email);
+
+        const submit = '[data-testid="joinEmailListSubmit"]';
+        await page.waitForSelector(submit);
+        await page.click(submit);
+
+        const mailchimp = ".bodyContent";
+        await page.waitForSelector(mailchimp);
+        const mailchimpContent = await page.$eval(
+          mailchimp,
+          el => el.innerText
+        );
+        expect(mailchimpContent).toContain("Subscription Confirmed");
       });
     }
-  };
 
-  testRoutes();
-});
+    test("can join courses", async () => {
+      await page.goto(routes.course);
 
-describe("subscribing", () => {
-  if (isProduction) {
-    test("can join the email list", async () => {
-      await page.goto(routes.home);
-
-      const button = '[data-testid="joinEmailListButton"]';
+      const button = '[data-testid="joinCourseButton"]';
       await page.waitForSelector(button);
       await page.click(button);
 
-      const input = '[data-testid="joinEmailListInput"]';
-      await page.waitForSelector(input);
-      await page.type(input, user.email);
-
-      const submit = '[data-testid="joinEmailListSubmit"]';
-      await page.waitForSelector(submit);
-      await page.click(submit);
-
-      const mailchimp = ".bodyContent";
-      await page.waitForSelector(mailchimp);
-      const mailchimpContent = await page.$eval(mailchimp, el => el.innerText);
-      expect(mailchimpContent).toContain("Subscription Confirmed");
+      expect(page.url()).toContain("https://sso.teachable.com");
     });
-  }
-
-  test("can join courses", async () => {
-    await page.goto(routes.course);
-
-    const button = '[data-testid="joinCourseButton"]';
-    await page.waitForSelector(button);
-    await page.click(button);
-
-    expect(page.url()).toContain("https://sso.teachable.com");
   });
-});
 
-describe("hosting", () => {
-  if (isProduction) {
-    test("redirects http to https", async () => {
-      await page.goto("http://trevordmiller.com");
-      expect(page.url()).toContain("https://trevordmiller.com");
-    });
+  describe("hosting", () => {
+    if (isProduction) {
+      test("redirects http to https", async () => {
+        await page.goto("http://trevordmiller.com");
+        expect(page.url()).toContain("https://trevordmiller.com");
+      });
 
-    test("redirects www to non-www", async () => {
-      await page.goto("https://www.trevordmiller.com");
-      expect(page.url()).toContain("https://trevordmiller.com");
-    });
-  }
-});
+      test("redirects www to non-www", async () => {
+        await page.goto("https://www.trevordmiller.com");
+        expect(page.url()).toContain("https://trevordmiller.com");
+      });
+    }
+  });
 
-describe("performance", () => {
-  if (!isLocal) {
-    test("loads in less than 1 second on current network", async () => {
-      await page.goto(routes.home);
-      const metrics = await page.metrics();
-      const browserTasksSeconds = metrics.TaskDuration;
-      expect(browserTasksSeconds).toBeLessThan(1.0);
-    });
-
-    test(
-      "loads in less than 3 seconds on throttled slower network",
-      async () => {
-        const client = await page.target().createCDPSession();
-        await client.send("Network.emulateNetworkConditions", {
-          offline: false,
-          downloadThroughput: (1.5 * 1024 * 1024) / 8,
-          uploadThroughput: (750 * 1024) / 8,
-          latency: 40
-        });
-
+  describe("performance", () => {
+    if (!isLocal) {
+      test("loads in less than 1 second on current network", async () => {
         await page.goto(routes.home);
         const metrics = await page.metrics();
         const browserTasksSeconds = metrics.TaskDuration;
-        expect(browserTasksSeconds).toBeLessThan(3.0);
-      },
-      15000
-    );
-  }
+        expect(browserTasksSeconds).toBeLessThan(1.0);
+      });
+
+      test(
+        "loads in less than 3 seconds on throttled slower network",
+        async () => {
+          const client = await page.target().createCDPSession();
+          await client.send("Network.emulateNetworkConditions", {
+            offline: false,
+            downloadThroughput: (1.5 * 1024 * 1024) / 8,
+            uploadThroughput: (750 * 1024) / 8,
+            latency: 40
+          });
+
+          await page.goto(routes.home);
+          const metrics = await page.metrics();
+          const browserTasksSeconds = metrics.TaskDuration;
+          expect(browserTasksSeconds).toBeLessThan(3.0);
+        },
+        15000
+      );
+    }
+  });
 });
 
 afterAll(() => {
